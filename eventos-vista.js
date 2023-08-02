c03ae46f0db2791a5fab66beea7b546300ef774e
Nicolas Pauer <89319674+NicoPauer@users.noreply.github.com>
2023-08-02 01:32:12 -0300

    Update eventos-vista.js
    Estructura básica empezando de vista

--- eventos-vista.js
+++ eventos-vista.js
@@ -1,1 +1,12 @@
+/*
+  Autor: © 2023 Nicolas Pauer
+  mail: nicolaspauer20@gmail.com
 
+  interfaz grafica para la vista de shows en una pagina que lista los de
+  cada semana: hay una vista general, una por categorias y una de cada
+  evento
+*/  
+// EN DESARROLLO
+const general = document.querySelector("section");
+
+general.setAttribute("class", "eventos");
